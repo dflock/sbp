75c2bb12a15a7d6cc5f57d92c4a897765fdb6bbf
Duncan Lock <dlock@phemi.com>
2016-03-14 16:02:58 -0700

    Updated the git segment to use the git-prompt.sh
    - Will use the `git-prompt.sh` script, if it's available,
      fallback to the current code.
    - Added new setting for the `git-promtp.sh` script location

--- segments/git.bash
+++ segments/git.bash
@@ -1,13 +1,23 @@
 ### Defaults
 _sbp_git_color_bg=${_sbp_git_color_bg:-$_sbp_color_green}
 _sbp_git_color_fg=${_sbp_git_color_fg:-$_sbp_color_dgrey}
+_sbp_git_prompt_location=${_sbp_git_promt_location:-'/usr/share/git/completion/git-prompt.sh'}
 
 function _sbp_generate_git_segment() {
-  [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]] || return 0
-  local git_head git_state git_value
-  git_head=$(sed -e 's,.*/\(.*\),\1,' <(git symbolic-ref HEAD 2>/dev/null || git rev-parse --short HEAD))
-  git_state=" $(git status --porcelain | sed -Ee 's/^(.M|M.|.R|R.) .*/\*/' -e 's/^(.A|A.) .*/\+/' -e 's/^(.D|D.) .*/\-/' | grep -oE '^(\*|\+|\?|\-)' | sort -u | tr -d '\n')"
-  git_value="${git_head}${git_state}"
+  if [ -f "$_sbp_git_prompt_location" ]; then
+    source "$_sbp_git_prompt_location"
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWUPSTREAM=1
+    GIT_PS1_DESCRIBE_STYLE="branch"
+    GIT_PS1_SHOWUPSTREAM="auto git"
+    git_value=$(__git_ps1 '%s')
+  else
+    [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]] || return 0
+    local git_head git_state git_value
+    git_head=$(sed -e 's,.*/\(.*\),\1,' <(git symbolic-ref HEAD 2>/dev/null || git rev-parse --short HEAD))
+    git_state=" $(git status --porcelain | sed -Ee 's/^(.M|M.|.R|R.) .*/\*/' -e 's/^(.A|A.) .*/\+/' -e 's/^(.D|D.) .*/\-/' | grep -oE '^(\*|\+|\?|\-)' | sort -u | tr -d '\n')"
+    git_value="${git_head}${git_state}"
+  fi
 
   _sbp_segment_new_color_fg="$_sbp_git_color_fg"
   _sbp_segment_new_color_bg="$_sbp_git_color_bg"
@@ -15,3 +25,6 @@ function _sbp_generate_git_segment() {
   _sbp_segment_new_value=" ${git_value} "
   _sbp_segment_new_create
 }
+
+
+# git_value=''$git_value
