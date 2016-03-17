9a10364851118f0690b4fcd0ac8f114e6a3e0511
Duncan Lock <duncan.lock@gmail.com>
2016-03-16 22:55:26 -0700

    Fixed colors & only output if in a git repo
    - Fixed dirty background
    - Only create segment if inside a git repo

--- segments/git.bash
+++ segments/git.bash
@@ -5,10 +5,13 @@ _sbp_git_color_clean_fg=${_sbp_repo_color_clean_fg:-$_sbp_color_dgrey}
 _sbp_git_color_dirty_bg=${_sbp_repo_color_dirty_bg:-$_sbp_color_pink}
 _sbp_git_color_dirty_fg=${_sbp_repo_color_dirty_fg:-$_sbp_color_white}
 
+
+
 function _sbp_generate_git_segment() {
-  local color_fg color_bg
+  local color_fg color_bg output_seg
   color_fg=$_sbp_git_color_clean_fg
   color_bg=$_sbp_git_color_clean_bg
+  output_seg=0
 
   if [ -f "$_sbp_git_prompt_location" ]; then
     source "$_sbp_git_prompt_location"
@@ -19,24 +22,31 @@ function _sbp_generate_git_segment() {
     GIT_PS1_SHOWUPSTREAM="auto verbose"
     git_value=$(__git_ps1 '%s')
 
-    regex='\>|\<|\*|\+'
-    if [[ $git_value =~ $regex ]]; then
-      color_fg=$_sbp_git_color_dirty_fg
-      color_bg=$_sbp_git_color_dirty_bg
+    if [[ ! -z "${git_value// }" ]]; then
+      regex='\>|\<|\*|\+'
+      if [[ $git_value =~ $regex ]]; then
+        color_fg=$_sbp_git_color_dirty_fg
+        color_bg=$_sbp_git_color_dirty_bg
+      fi
+
+      git_value=' '$git_value
+      output_seg=1
     fi
 
-    git_value=' '$git_value
   else
     [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]] || return 0
     local git_head git_state git_value
     git_head=$(sed -e 's,.*/\(.*\),\1,' <(git symbolic-ref HEAD 2>/dev/null || git rev-parse --short HEAD))
     git_state=" $(git status --porcelain | sed -Ee 's/^(.M|M.|.R|R.) .*/\*/' -e 's/^(.A|A.) .*/\+/' -e 's/^(.D|D.) .*/\-/' | grep -oE '^(\*|\+|\?|\-)' | sort -u | tr -d '\n')"
     git_value="${git_head}${git_state}"
+    output_seg=1
   fi
 
-  _sbp_segment_new_color_fg="$color_fg"
-  _sbp_segment_new_color_bg="$color_bg"
-  _sbp_segment_new_length="$(( ${#git_value} + 0 ))"
-  _sbp_segment_new_value=" ${git_value} "
-  _sbp_segment_new_create
+  if [[ "$output_seg" -eq 1 ]]; then
+    _sbp_segment_new_color_fg="$color_fg"
+    _sbp_segment_new_color_bg="$color_bg"
+    _sbp_segment_new_length="$(( ${#git_value} + 0 ))"
+    _sbp_segment_new_value=" ${git_value} "
+    _sbp_segment_new_create
+  fi
 }
