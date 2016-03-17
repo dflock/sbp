714eb665dbcd1af3558a430cddd38f10c342151b
Duncan Lock <dlock@phemi.com>
2016-03-16 18:58:05 -0700

    Updated colours & made clean/dirty repo colors configurable

--- segments/git.bash
+++ segments/git.bash
@@ -1,16 +1,31 @@
 ### Defaults
-_sbp_git_color_bg=${_sbp_git_color_bg:-$_sbp_color_green}
-_sbp_git_color_fg=${_sbp_git_color_fg:-$_sbp_color_dgrey}
 _sbp_git_prompt_location=${_sbp_git_promt_location:-'/usr/share/git/completion/git-prompt.sh'}
+_sbp_git_color_clean_bg=${_sbp_repo_color_clean_bg:-$_sbp_color_green}
+_sbp_git_color_clean_fg=${_sbp_repo_color_clean_fg:-$_sbp_color_dgrey}
+_sbp_git_color_dirty_bg=${_sbp_repo_color_dirty_bg:-$_sbp_color_pink}
+_sbp_git_color_dirty_fg=${_sbp_repo_color_dirty_fg:-$_sbp_color_white}
 
 function _sbp_generate_git_segment() {
+  local color_fg color_bg
+  color_fg=$_sbp_git_color_clean_fg
+  color_bg=$_sbp_git_color_clean_bg
+
   if [ -f "$_sbp_git_prompt_location" ]; then
     source "$_sbp_git_prompt_location"
     GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWUPSTREAM=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
     GIT_PS1_DESCRIBE_STYLE="branch"
-    GIT_PS1_SHOWUPSTREAM="auto git"
+    # GIT_PS1_SHOWUPSTREAM=1
+    GIT_PS1_SHOWUPSTREAM="auto verbose"
     git_value=$(__git_ps1 '%s')
+
+    regex='\>|\<|\*|\+'
+    if [[ $git_value =~ $regex ]]; then
+      color_fg=$_sbp_git_color_dirty_fg
+      color_bg=$_sbp_git_color_dirty_bg
+    fi
+
+    git_value=' '$git_value
   else
     [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]] || return 0
     local git_head git_state git_value
@@ -19,12 +34,9 @@ function _sbp_generate_git_segment() {
     git_value="${git_head}${git_state}"
   fi
 
-  _sbp_segment_new_color_fg="$_sbp_git_color_fg"
-  _sbp_segment_new_color_bg="$_sbp_git_color_bg"
+  _sbp_segment_new_color_fg="$color_fg"
+  _sbp_segment_new_color_bg="$color_bg"
   _sbp_segment_new_length="$(( ${#git_value} + 0 ))"
   _sbp_segment_new_value=" ${git_value} "
   _sbp_segment_new_create
 }
-
-
-# git_value=''$git_value
